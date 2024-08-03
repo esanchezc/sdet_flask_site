4a96cda9abff557ea72b9518afdff0b16e3b2935
Emanuel Sanchez <esanchezc.sqa@gmail.com>
2024-08-03 16:50:36 -0500

    Add work experiences

--- server.py
+++ server.py
@@ -1,18 +1,29 @@
+from flask import Flask, render_template, g
 import json
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+def load_cv():
+    if 'cv' not in g:
+        with open("cv.json", 'r') as file:
+            g.cv = json.load(file)
+    return g.cv
+
+@app.before_request
+def before_request():
+    g.cv = load_cv()
 
 @app.route("/emanuel")
-def cv():
-    with open("cv.json", 'r') as file:
-        file_contents = file.read()
-        cv = json.loads(file_contents)    
-    return render_template('emanuel.html', summary=cv['summary'])
+def main_page():
+    return render_template('emanuel.html', cv=g.cv)
+
+@app.route("/experience/<int:company_index>")
+def get_experience(company_index):
+    try:
+        experience = g.cv['work_experience'][company_index]
+        return render_template('experience.html', experience=experience)
+    except IndexError:
+        return "Experience not found", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
